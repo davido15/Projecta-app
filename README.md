e742dae4e9f147f213af1bb0235b4c7dee60c8c1
David Dornyoh <daviddors12@gmail.com>
2025-07-07 23:13:09 +0000

    Initial commit: Add hello-world-react app

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World React App
+This is a simple React app that displays "Hello World 🌍". Ready to deploy on Render.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World 🌍</h1>
+      <p>Welcome to React deployed on Render</p>
+    </div>
+  );
+}
+
+export default App;
